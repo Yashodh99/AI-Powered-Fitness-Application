47eb8ea175e0f6d3def32f0b2bbd965ff5861224
yash.fern99 <yash.fern99@gmail.com>
2025-09-25 18:31:27 +0530

    Created post and get APIs

---
+++ userservice/src/main/java/com/fitness/userservice/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.fitness.userservice.controller;
+
+import com.fitness.userservice.dto.RegisterRequestDto;
+import com.fitness.userservice.dto.UserResponseDto;
+import com.fitness.userservice.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+
+
+    private UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponseDto> getUserProfile(@PathVariable String userId){
+        return ResponseEntity.ok(userService.getUserProfile(userId));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> register(@Valid @RequestBody RegisterRequestDto request){
+        return ResponseEntity.ok(userService.register(request));
+    }
+}
