60be3bc11cbb0ef2c7e657fbe1d482ee52ec1e59
yash.fern99 <yash.fern99@gmail.com>
2025-10-12 20:26:11 +0530

    fixed errors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,69 +1,143 @@
-import {BrowserRouter as Router , Navigate,Route, Routes, useLocation} from "react-router";
-import Button from '@mui/material/Button';
-import { useContext } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import {AuthContext} from 'react-oauth2-code-pkce';
-import {useDispatch} from 'react-redux';
-import {setCredentials} from './store/authSlice';
+// import {BrowserRouter as Router , Navigate,Route, Routes, useLocation} from "react-router";
+// import Button from '@mui/material/Button';
+// import { useContext } from "react";
+// import { useEffect } from "react";
+// import { useState } from "react";
+// import {AuthContext} from 'react-oauth2-code-pkce';
+// import {useDispatch} from 'react-redux';
+// import {setCredentials} from './store/authSlice';
+// import Box from "@mui/material/Box";
+// import ActivityForm from "./components/ActivityForm";
+// import ActivityList from "./components/ActivityList";
+// import ActivityDetail from "./components/ActivityDetail";
+
+
+
+
+
+// const ActivitiesPage = () => {
+//   return(
+//   <Box sx={{p:2, border:'1pz dashed grey'}}>
+//   <ActivityForm onActivitiesAdded = {()=> window.location.reload()} />
+//   <ActivityList />
+//   </Box>);
+// }
+
+// function App() {
+
+
+//   const{token, toeknData, logIn, logOut, isAuthenticated} = useContext(AuthContext);
+//   const dispatch = useDispatch();
+
+//   const[authReady, setAuthReady] = useState(false);
+//   useEffect(()=> {
+//     if(token){
+//       dispatch(setCredentials({token,user:toeknData}));
+//       setAuthReady(true);
+
+//     }
+//   },[token,toeknData,dispatch]);
+//   return (
+//     <Router>
+//       {!token ? (
+//        <Button variant = "cantained" color ="#dc004e"
+//        onClick={() =>{
+//         logIn();
+//        }}> LOGIN </Button>
+//       ):(
+       
+
+//          <Box sx={{ p: 2, border: '1px dashed grey' }}>
+//           <Button variant = "contained" color ="secondary"
+//        onClick={logOut}>
+       
+//         Logout </Button>
+//       <Routes>
+//         <Route path="/activities" element={<ActivitiesPage/>}/>
+//         <Route path="/activities/:id" element={<ActivityDetail/>}/>
+
+//         <Route path ="/" element={token ? <Navigate to="/activities" replace/> : <div>Welcome Please Login.</div>} />
+//       </Routes>
+//     </Box>
+
+//       )}
+//     </Router>
+    
+//   )
+// }
+
+// export default App
+
+
+
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router";
+import Button from "@mui/material/Button";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./store/authSlice";
 import Box from "@mui/material/Box";
 import ActivityForm from "./components/ActivityForm";
 import ActivityList from "./components/ActivityList";
 import ActivityDetail from "./components/ActivityDetail";
 
-
-
-
-
 const ActivitiesPage = () => {
-  return(
-  <Box sx={{p:2, border:'1pz dashed grey'}}>
-  <ActivityForm onActivitiesAdded = {()=> window.location.reload()} />
-  <ActivityList />
-  </Box>);
-}
+  return (
+    <Box sx={{ p: 2, border: "1px dashed grey" }}>
+      <ActivityForm onActivityAdded={() => window.location.reload()} />
+      <ActivityList />
+    </Box>
+  );
+};
 
 function App() {
-
-
-  const{token, toeknData, logIn, logOut, isAuthenticated} = useContext(AuthContext);
+ 
+  const { token, tokenData, login, logoff, isAuthenticated } = useContext(AuthContext);
   const dispatch = useDispatch();
 
-  const[authReady, setAuthReady] = useState(false);
-  useEffect(()=> {
-    if(token){
-      dispatch(setCredentials({token,user:toeknData}));
-      setAuthReady(true);
+  const [authReady, setAuthReady] = useState(false);
 
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
     }
-  },[token,toeknData,dispatch]);
+  }, [token, tokenData, dispatch]);
+
   return (
     <Router>
       {!token ? (
-       <Button variant = "cantained" color ="#dc004e"
-       onClick={() =>{
-        logIn();
-       }}> LOGIN </Button>
-      ):(
-       
+        <Button
+          variant="contained"    
+          color="secondary"      
+          onClick={() => {
+            login();              
+          }}
+        >
+          LOGIN
+        </Button>
+      ) : (
+        <Box sx={{ p: 2, border: "1px dashed grey" }}>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={logoff}      
+          >
+            Logout
+          </Button>
 
-         <Box sx={{ p: 2, border: '1px dashed grey' }}>
-          <Button variant = "contained" color ="secondary"
-       onClick={logOut}>
-       
-        Logout </Button>
-      <Routes>
-        <Route path="/activities" element={<ActivitiesPage/>}/>
-        <Route path="/activities/:id" element={<ActivityDetail/>}/>
-
-        <Route path ="/" element={token ? <Navigate to="/activities" replace/> : <div>Welcome Please Login.</div>} />
-      </Routes>
-    </Box>
-
+          <Routes>
+            <Route path="/activities" element={<ActivitiesPage />} />
+            <Route path="/activities/:id" element={<ActivityDetail />} />
+            <Route
+              path="/"
+              element={token ? <Navigate to="/activities" replace /> : <div>Welcome Please Login.</div>}
+            />
+          </Routes>
+        </Box>
       )}
     </Router>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,33 +1,36 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api';
-
+const API_URL = "http://localhost:8080/api";
 
 const api = axios.create({
-    baseURL:API_URL
+  baseURL: API_URL,
 });
 
 
-api.interceptors.request.use((config) =>{
-    const userID = localStorage.getItem('userId');
-    const token = localStorage.getItem('token');
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
 
-    if(token){
-        config.headers['Authorization'] = `Bearere ${token}`;
+    config.headers = config.headers ?? {};
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
-    if(userId){
-        config.headers['X=User-ID'] = userId;
+
+    if (userId) {
+      config.headers["X-User-ID"] = userId;
     }
+
     return config;
-
-}
+  },
+  (error) => Promise.reject(error)
 );
 
 
-export const getActivities = () => api.get('/activities');
-export const addActivity = (actvity) => api.post('/activity', activity);
-export const getActivityDetail = (id) => api.get(`/recommendations/activity/${id}`);
+export const getActivities = () => api.get("/activities");
 
+export const addActivity = (activity) => api.post("/activities", activity);
 
-
-
+export const getActivityDetail = (id) =>
+  api.get(`/recommendations/activity/${id}`);
